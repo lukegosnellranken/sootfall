bb14fad1ac98358215ba822832a20ea01bca0438
Gosnell <gosnellwebdesign@gmail.com>
2025-08-12 19:19:21 -0500

    style updates

--- src/components/home-component/HomeComponent.js
+++ src/components/home-component/HomeComponent.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
 import './HomeComponent.scss';
+import { useSettings } from "../../Settings";
 import HomeCard from "../home-card/HomeCard";
 import HomeSideCard from "../home-side-card/HomeSideCard";
 import HomeMobileCard from "../home-mobile-card/HomeMobileCard";
-import wuthering from '../../images/wuthering.jpg';
+import wutheringLight from '../../images/wuthering-side-light.jpg';
+import wutheringDark from '../../images/wuthering-side-dark.jpg';
 import beans from '../../images/coffee-beans.png';
 
 function HomeComponent() {
+    const { theme } = useSettings(); // Instantiate theme object from Settings.js
+    // Since lightTheme is computed from other reactive data (theme.title), only recalcualte it when theme changes
+    const lightTheme = useMemo(() => theme.title === "Light", [theme]);;
+
     return (
         <div id="homecomponent-container">
             <div id="div-homecomponent-card">
@@ -18,14 +24,14 @@ function HomeComponent() {
             <div id="div-homecomponent-mobile-card">
                 <HomeMobileCard
                     title1 = "Burning the Night"
-                    image1 = {wuthering}
+                    image1 = {lightTheme ? wutheringLight : wutheringDark}
                     description1 = "Sootfall is a collective of prose and poetry writers seeking to understand and put to order the unending enigma of being through the God-given craft of thoughtful expression."
                 />
             </div>
             <div id="div-homecomponent-side-card">
                 <HomeSideCard
                     title1 = "Burning the Night"
-                    image1 = {wuthering}
+                    image1 = {lightTheme ? wutheringLight : wutheringDark}
                     description1 = "Sootfall is a collective of prose and poetry writers seeking to understand and put to order the unending enigma of being through the God-given craft of thoughtful expression."
                     title2 = "Buy Us a Coffee"
                     image2 = {beans}
