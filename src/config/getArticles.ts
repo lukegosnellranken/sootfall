2d143a48c522283d3c695ba011b9737ef810edae
Gosnell <gosnellwebdesign@gmail.com>
2025-12-14 04:21:19 -0600

    ISR treatment for getArticles.ts

--- src/config/getArticles.ts
+++ src/config/getArticles.ts
@@ -4,47 +4,51 @@ interface Article {
   title: string;
   dateString: string;
   image: string;
-  tags: any; 
+  tags: string[];
   author: string;
 }
 
 async function getArticles(fetchEndpoint: string) {
-    let articles: Article[] = [];
-    try {
-        const res = await fetch(fetchEndpoint, { headers: { 
-            'Authorization': `Bearer ${token}`,
-            // Prevents AWS caching API responses -- only fetch updated content
-            'Cache-Control': 'no-cache'  
-        } });
-        if (res.ok) {
-            const data = await res.json();
-            articles = data.data.map((item: any) => {
-                let dateString = item.date.slice(5) + "-" + item.date.slice(2, 4);
-                let image;
-                if (envName === 'cloud') {
-                    image = item.image.formats.medium.url;
-                } else {
-                    image = backendLink + item.image.formats.medium.url;
-                }
-                let tagsArray: string[] = [];
-                if (item.tags != null) {
-                    tagsArray = item.tags.split(",").map((tag: string) => tag.trim());
-                }
-                return {
-                    title: item.title,
-                    dateString: dateString,
-                    image: image,
-                    tags: tagsArray,
-                    author: item.author.name
-                };
-            });
-        } else {
-            console.log('Articles res error');
-        }
-    } catch (error) {
-        console.log(error);
+  let articles: Article[] = [];
+  try {
+    const res = await fetch(fetchEndpoint, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      // Let Next.js cache and revalidate this fetch every 60 seconds
+      next: { revalidate: 60 },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      articles = data.data.map((item: any) => {
+        const dateString = item.date.slice(5) + '-' + item.date.slice(2, 4);
+
+        const image =
+          envName === 'cloud'
+            ? item.image.formats.medium.url
+            : backendLink + item.image.formats.medium.url;
+
+        const tagsArray =
+          item.tags != null
+            ? item.tags.split(',').map((tag: string) => tag.trim())
+            : [];
+
+        return {
+          title: item.title,
+          dateString,
+          image,
+          tags: tagsArray,
+          author: item.author.name,
+        };
+      });
+    } else {
+      console.error('Articles res error');
     }
-    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+  return articles;
 }
 
 export { getArticles };
