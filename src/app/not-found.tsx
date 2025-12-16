c807349779e399f58c421c474db706735b894d56
Gosnell <gosnellwebdesign@gmail.com>
2025-12-16 12:55:07 -0600

    not-found documentation

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
-// This file defines the component that is displayed when a page cannot be found.
-// It provides a user-friendly message indicating that no page was found.
+// This file defines the `NotFound` component.
+// It is displayed when a user navigates to a route that does not exist.
+// The component provides a simple, user-friendly 404 error message.
 
 // We're importing necessary components and hooks here.
 import React from "react";
 import './notfound.scss';
 
+// The `NotFound` component renders a 404 page.
+// It uses a heading and a short message to inform the user that the page was not found.
 function NotFound() {
     return(
         <div id="notFound">
